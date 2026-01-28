d74eb909c9c77045651d1c7ba60d4244cc6dfdc9
Amit Kumar Sahu Gupta <Amitkumarsahugupta@gmail.com>
2026-01-28 13:09:07 +0000

    Post me aapas me Gap 5 px ka hona chahiye  (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="container mx-auto py-10 px-4">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2">
-            <div className="grid grid-cols-2 gap-8">
+            <div className="grid grid-cols-2 gap-2">
               {posts.map((post) => (
                 <PostCard key={post.id} post={post} />
               ))}
